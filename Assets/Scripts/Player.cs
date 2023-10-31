d58193a1aa73e44a7d2e240d4e76ece3a6d27407
PrinceMalie <73131545+PrinceMalie@users.noreply.github.com>
2023-10-31 09:46:59 -0500

    Added keys to screen
    Added keys to screen. Still having issues with my collider.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    float speed = 5.0f; 
+    float speed = 5.0f;
     // Start is called before the first frame update
     void Start()
     {
@@ -34,21 +34,24 @@ public class Player : MonoBehaviour
 
     void OnCollisionEnter (Collision collision)
     {
-        if (Input.GetKeyDown("left"))
+        if (collision.gameObject.tag == "walls")
         {
-            transform.Translate(speed * Time.deltaTime, 0, 0);
-        }
-        if (Input.GetKeyDown("right"))
-        {
-            transform.Translate(-speed * Time.deltaTime, 0, 0);
-        }
-        if (Input.GetKeyDown("up"))
-        {
-            transform.Translate(0, -speed * Time.deltaTime, 0);
-        }
-        if (Input.GetKeyDown("down"))
-        {
-            transform.Translate(0, speed * Time.deltaTime, 0);
+            if (Input.GetKeyDown("left"))
+            {
+                transform.Translate(speed * Time.deltaTime, 0, 0);
+            }
+            if (Input.GetKeyDown("right"))
+            {
+                transform.Translate(-speed * Time.deltaTime, 0, 0);
+            }
+            if (Input.GetKeyDown("up"))
+            {
+                transform.Translate(0, -speed * Time.deltaTime, 0);
+            }
+            if (Input.GetKeyDown("down"))
+            {
+                transform.Translate(0, speed * Time.deltaTime, 0);
+            }
         }
     }
 }
